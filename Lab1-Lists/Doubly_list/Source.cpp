6ed9b78428546dc9930e86335c4e7c0161ea9adb
Stanislav <31759900+stanislav7766@users.noreply.github.com>
2019-05-23 17:30:00 +0300

    Doubly List

--- Lab1-Lists/Doubly_list/Source.cpp
+++ Lab1-Lists/Doubly_list/Source.cpp
@@ -4,28 +4,31 @@
 using namespace std;
 
 template<typename T>
-struct Node {//элемент списка
+struct Node {
 	T data;
 	Node *pNext;
+	Node *pPrev;
 };
 
 template<typename T>
 class List {
 private:
 	Node<T> *head;
+	Node<T> *tail;
 	int Size;
 public:
 	List();
 	~List();
-	void isEmpty();
-	void insert(T data, int n);
-	void removeAt(int n);
 	void push_front(T data);
 	void push_back(T data);
-	int GetSize() { return Size; }
-	T& operator[](const int index);
-	void clear();
 	void pop_front();
+	void pop_back();
+	void insert(T data, int n);
+	void removeAt(int n);
+	T& operator[](const int index);
+	bool isEmpty();
+	int GetSize() { return Size; }
+	void clear();
 	void show();
 
 };
@@ -33,178 +36,249 @@ public:
 template<typename T>
 List<T>::List() {
 	Size = 0;
-	head = nullptr;
+	head = tail = nullptr;
 }
+
 
 template<typename T>
 List<T>::~List() {
 	clear();
 }
 
-template<typename T>
-void List<T>::isEmpty()
-{
-	if (head == nullptr) {
-		cout << "Empty";
-	}
-	else{
-		cout << "Not empty";
-	}
-}
-
-template<typename T>
-void List<T>::insert(T data, int n)
-{
-	Node<T> * node = new Node<T>;
-	Node<T> *current = this->head;
-	for (int counter = 0; current != nullptr; counter++)
-	{
-		if (counter == n)//если текущ эл = индексу
-		{/*
-			Node<T> *temp = current->pNext;
-			current->pNext = node;
-			node->data = data;
-			node->pNext = temp;*/
-			node->pNext = current->pNext;
-			node->data = data;
-			current->pNext = node;
-
-		}
-		current = current->pNext;
-	}
-	Size++;
-
-}
-
-
-template<typename T>
-void List<T>::removeAt(int n)
-{
-	Node<T> *current = this->head;
-	for (int counter = 0; current != nullptr; counter++)
-	{
-		if (counter == n - 1)
-		{
-			Node<T> *temp = current->pNext;
-			current->pNext = temp->pNext;
-			delete temp;
-		}
-		current = current->pNext;
-	}
-	Size--;
-}
-
-
-
-
-template<typename T>
-void List<T>::push_front(T data) {
-	Node<T> *node = new Node<T>;//создаем элем
-	Node<T> *temp = head;//помещаем в временный эл , наш head
-	head = node;//в head присваиваем наш элем
-	node->data = data;// в адресс дата присваиваем дата
-	node->pNext = temp;//в след адресс элем , присваиваем старый head
-	Size++;
-}
-
 
 template<typename T>
 void List<T>::push_back(T data) {
-	Node<T> *node = new Node<T>;//создаем элем
-	if (head == nullptr) {//список пуст
-		head = node;//в head присваиваэм наш эл
-		head->data = data;//в адресс дата присваиваем дата
-		head->pNext = nullptr;//след адресс на элем нул
-	}
-	else {
-		Node<T> *temp = this->head;//в временный эл присваиваем ссылку на head
-		while (temp->pNext != nullptr) {//пока след адресс эл не нул
-			temp = temp->pNext;// двигаемся на сл эл
-		}
-		temp->pNext = node;//в следующий адресс временного эл, присваиваем наш элем 
-		node->data = data;//
-		node->pNext = nullptr;//
-	}
+
+	Node<T> *node = new Node<T>;
+
+	node->pNext = nullptr;
+	node->data = data;
+	node->pPrev = tail;
+
+	if (tail != nullptr) tail->pNext = node;
+	if (Size == 0) head = tail = node;
+	else tail = node;
 	Size++;
 }
 
-
 template<typename T>
-void List<T>::show() {
-	if (head != nullptr) {//список не пуст
-		Node<T> *temp = head;//в временный элем присваиваем head
-		while (temp->pNext != nullptr) {//след адресс вр элем не нул
-			cout << temp->data << ", ";//водим данные вр элем
-			temp = temp->pNext;//двигаемся на сл эл
-		}
-		cout << temp->data << endl;//когда temp->pNext = nullptr, выводим его дата
-	}
+void List<T>::push_front(T data) {
+	Node<T> *node = new Node<T>;
+	node->pPrev = nullptr;
+	node->data = data;
+	node->pNext = head;
 
-}
-
-
-template <typename T>
-void List<T>::clear() {
-	while (Size) {
-		pop_front();
-
-	}
-	cout << "List was cleared" << endl;
+	if (head != nullptr) head->pPrev = node;
+	if (Size == 0) head = tail = node;
+	else head = node;
+	Size++;
 }
 
 
 template <typename T>
 void List<T>::pop_front() {
-	Node<T> *temp = head;//в вр элем присваиваем head 
-	head = head->pNext;//в head присваиваем след адресс элем
-	delete temp;//удаляем указатель
+	Node<T> *temp = head;
+	head = head->pNext;
+	delete temp;
+	Size--;
+}
+
+
+template <typename T>
+void List<T>::pop_back() {
+	Node<T> *temp = tail;
+	tail = tail->pPrev;
+	temp->pPrev = nullptr;
+	tail->pNext = nullptr;
+	delete temp;
+	Size--;
+}
+
+
+template<typename T>
+void List<T>::insert(T data, int index)
+{
+	if (index < 0)
+	{
+		cout << "Invalid index\n";
+		return;
+	}
+	if (index >= Size - 1) {
+		push_back(data);
+		return;
+	}
+	if (index <= Size / 2)
+	{
+		cout << "index<\n";
+		Node<T> *current = this->head;
+		for (int counterLtoR = 0; current != nullptr; counterLtoR++)
+		{
+			if (counterLtoR == index)
+			{
+				Node<T> * node = new Node<T>;
+				node->pNext = current->pNext;
+				node->pPrev = current;
+				current->pNext = node;
+				node->data = data;
+				current->pNext->pPrev = node;
+				break;
+			}
+			current = current->pNext;
+		}
+	}
+	else
+	{
+		cout << "index>\n";
+		Node<T> *current = this->tail;
+		for (int counterRtoL = Size - 1; current != nullptr; counterRtoL--)
+		{
+			if (counterRtoL ==index)
+			{
+				Node<T> * node = new Node<T>;
+				node->pNext = current->pNext;
+				node->pPrev = current;
+				current->pNext = node;
+				node->data = data;
+				current->pNext->pPrev = node;
+				break;
+			}
+			current = current->pPrev;
+		}
+	}
+	Size++;
+}
+
+
+template<typename T>
+void List<T>::removeAt(int index)
+{
+	if (index > Size-1 || index < 0) {
+		cout << "Invalid index\n";
+		return;
+	}
+	if (index == 0) {
+		pop_front();
+		return;
+	}	
+	if (index == Size-1) {
+		pop_back();
+		return;
+	}
+    if (index <= (Size) / 2) {
+		cout << "index<\n";
+		Node<T> *current = this->head;
+		for (int counterLtoR = 0; current != nullptr; counterLtoR++)
+		{
+			if (counterLtoR == index-1) {
+				Node<T> *temp = current->pNext;
+				current->pNext = temp->pNext;
+				temp->pNext->pPrev = current;
+				delete temp;
+				break;
+			}
+			current = current->pNext;
+		}
+	}
+	else {
+	    cout << "index>\n";
+		Node<T> *current = this->tail;
+		for (int counterRtoL = Size - 1; current != nullptr; counterRtoL--)
+		{
+			if (counterRtoL == index-1)
+			{
+				Node<T> *temp = current->pNext;
+				current->pNext = temp->pNext;
+				temp->pNext->pPrev = current;
+				delete temp;
+				break;
+			}
+			current = current->pPrev;
+		}
+	}
 	Size--;
 }
 
 template<typename T>
-T & List<T>::operator[](const int index)//итерирование списка ождения индекса и возвращения его значения)
+T & List<T>::operator[](const int index)
 {
-	if (Size == 0) { cout << "empty\n"; }
-	else
-	{
-		Node<T> *current = this->head;//наш временный блок, чтобы понять где мы находимся
-		for (int counterLtoR = 0; current != nullptr; counterLtoR++)//пока указатель не нал
+	if (Size == 0 || index < 0 || index > Size -1) {
+		cout << "invalid index\n";
+	}
+	else {
+		if (index < Size / 2)
 		{
-			if (counterLtoR == index)//если текущ эл = индексу
+			Node<T> *current = this->head;
+			for (int counterLtoR = 0; current != nullptr; counterLtoR++)
 			{
-				return current->data;  //вернем его поле дата
+				if (counterLtoR == index) { return current->data; }
+				current = current->pNext;
 			}
-			current = current->pNext; //если нет, двигаемся на сл эл
+		}
+		else
+		{
+			Node<T> *current1 = this->tail;
+			for (int counterRtoL = Size - 1; current1 != nullptr; counterRtoL--)
+			{
+				if (counterRtoL == index) { return current1->data; }
+				current1 = current1->pPrev;
+			}
 		}
 	}
 }
 
+
+template<typename T>
+bool List<T>::isEmpty()
+{
+	if (head == nullptr) { return true; }
+	else { return false; }
+}
+
+
+template <typename T>
+void List<T>::clear() {
+	while (Size) pop_front();
+	cout << "List was cleared" << endl;
+}
+
+
+template<typename T>
+void List<T>::show() {
+	if (Size != 0) {
+		Node<T> *current = head;
+		while (current->pNext != nullptr) {
+			cout << current->data << ", ";
+			current = current->pNext;
+		}
+		cout << current->data << endl;
+	}
+	else {
+		cout << "empty\n";
+	}
+}
+
+
+
 void main() {
 	setlocale(LC_ALL, "ru");
 	regex rx;
-
 	List<string> SHARP;
-	SHARP.push_front("tabletSHARP");
-	SHARP.push_back("mouseSHARP");
-	SHARP.push_back("notebookSHARP");
+	SHARP.push_back("0");
+	SHARP.push_back("1");
+	SHARP.push_back("2");
+	SHARP.push_back("3");
+	SHARP.push_back("4");
+	SHARP.push_back("5");
+	SHARP.insert("01", 5);
+	SHARP.removeAt(5);
+	SHARP.pop_front();
+	SHARP.pop_front();
+	SHARP.pop_front();
+	SHARP.pop_front();
+	SHARP.pop_front();
+	SHARP.pop_front();
+	SHARP.show();
 
-	List<string> SONY;
-	SONY.push_front("tabletSONY");
-	SONY.push_back("mouseSONY");
 
-	SONY.push_back("notebookSONY");
-
-	List<string> Common;
-	for (int i = 0; i < SHARP.GetSize(); i++)
-	{
-	for (int j = 0; j < SONY.GetSize(); j++)
-	{
-	rx = SONY[j].substr(0, 3);
-	if (regex_search(SHARP[i].begin(), SHARP[i].end(), rx) && regex_search(SONY[j].begin(), SONY[j].end(), rx))
-	Common.push_front(SHARP[i] + "-" + SONY[j]);
-	}
-	}
-
-	Common.show();
 	system("pause");
 }
